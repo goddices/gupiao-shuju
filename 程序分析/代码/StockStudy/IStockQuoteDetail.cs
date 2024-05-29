ca85ea17d950b9b285c389cd32cf16e4cb284e1c
goddi <goddi@DESKTOP-OMOBU7U>
2024-05-29 22:58:45 +0800

    程序分析

---
+++ 程序分析/代码/StockStudy/IStockQuoteDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockStudy
+{
+    public interface IStockQuote
+    {
+        public string Name { get; set; }
+        public IEnumerable<IStockQuoteDetail> Quotes { get; set; }
+    }
+
+    public interface IStockQuoteDetail
+    {
+        public DateTime TradeDay { get; set; }
+        public decimal Open { get; set; }
+        public decimal Close { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+    }
+}
---
+++ 程序分析/代码/StockStudy/InvestmentSummary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockStudy
+{
+    public class InvestmentSummary
+    {
+        public string Name { get; set; }
+
+        public int TotalDays { get; set; }
+
+        public decimal FinalAmount { get; set; }
+
+        public decimal CostAmount { get; set; }
+
+        public decimal Earnings => FinalAmount - CostAmount;
+
+        public decimal Rate => Earnings / CostAmount;
+
+        public override string ToString()
+        {
+            return $"投资{Name} {TotalDays}天（{(TotalDays / 365M).ToString("0.00")}年), 赚了{Earnings.ToString("0.00")}, 收益率 {(Rate * 100).ToString("0.00")}%, 市值 {FinalAmount.ToString("0.00")}, 成本 {CostAmount.ToString("0.00")}";
+        }
+    }
+}
